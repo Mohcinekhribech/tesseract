d97d8b72226ed49b8827fbf4362b23fb97a0de1a
mohcinekhribech <mohcinekhribche123@gmail.com>
2025-10-26 19:55:36 +0100

    nginx

--- prisma/migrations/20250910123039_create_admin_table/migration.sql
+++ prisma/migrations/20250910123039_create_admin_table/migration.sql
@@ -10,3 +10,6 @@ CREATE TABLE "public"."Admin" (
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Admin_email_key" ON "public"."Admin"("email");
+
+-- Insert default admin user
+INSERT INTO "public"."Admin" ("name", "email", "password") VALUES ('Admin tesseract', 'admin@tesseract.com', 'admintesseract123');
